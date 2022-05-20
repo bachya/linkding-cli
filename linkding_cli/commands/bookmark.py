68f11f19af58bfc9170a7a843d5319dcb4a66ef6
Aaron Bach <bachya1208@gmail.com>
2022-05-20 16:23:45 -0600

    Simplify command architecture (#9)

--- linkding_cli/commands/bookmark.py
+++ linkding_cli/commands/bookmark.py
@@ -1,19 +1,14 @@
-"""Define the bookmarks all command."""
+"""Define the bookmark command."""
 from __future__ import annotations
 
 import asyncio
 import json
-from typing import Any
 
 from aiolinkding.errors import LinkDingError
 import typer
 
 from linkding_cli.const import CONF_LIMIT, CONF_OFFSET, CONF_QUERY
 from linkding_cli.helpers.decorator import log_exception
-
-
-async def async_get_all_bookmarks() -> dict[str, Any]:
-    """Get all bookmarks."""
 
 
 @log_exception(LinkDingError)
@@ -56,12 +51,12 @@ def get_all(
             kwargs[conf_key] = param
 
     if archived:
-        coro = ctx.obj.client.bookmarks.async_get_archived(**kwargs)
+        func = ctx.obj.client.bookmarks.async_get_archived
     else:
-        coro = ctx.obj.client.bookmarks.async_get_all(**kwargs)
-    data = asyncio.run(coro)
+        func = ctx.obj.client.bookmarks.async_get_all
+    data = asyncio.run(func(**kwargs))
     typer.echo(json.dumps(data))
 
 
-BOOKMARK_ALL_APP = typer.Typer()
-BOOKMARK_ALL_APP.callback(invoke_without_command=True)(get_all)
+BOOKMARK_APP = typer.Typer()
+BOOKMARK_APP.command(name="all")(get_all)
